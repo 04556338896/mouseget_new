d6711677eb24ccec5cbaa3145e71c778dc9800be
04556338896 <1115729702@qq.com>
2020-10-05 21:00:04 +0800

    add code

---
+++ cmake-build-debug/CMakeFiles/mouseget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mouseget.dir/main.cpp.o"
+  "mouseget"
+  "mouseget.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mouseget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
